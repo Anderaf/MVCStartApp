5a5da3f7a7d49c614eb296e3e3ae12246bb9a7cd
Anderaf <kuldperea@gmail.com>
2023-06-17 21:06:08 +1000

    Add project files.

---
+++ MVCStartApp/Middleware/LoggingMiddleware.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+using System;
+using MVCStartApp.Repositories;
+using MVCStartApp.Models.Db;
+
+namespace MVCStartApp.Middleware
+{
+    public class LoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly IRequestRepository _repository;
+
+        /// <summary>
+        ///  Middleware-компонент должен иметь конструктор, принимающий RequestDelegate
+        /// </summary>
+        public LoggingMiddleware(RequestDelegate next, IRequestRepository request)
+        {
+            _next = next;
+            _repository = request;
+        }
+
+        /// <summary>
+        ///  Необходимо реализовать метод Invoke  или InvokeAsync
+        /// </summary>
+        public async Task InvokeAsync(HttpContext context)
+        {
+            string requestURL = $"[{DateTime.Now}]: New request to http://{context.Request.Host.Value + context.Request.Path}";
+            Console.WriteLine(requestURL);
+            await _repository.AddRequest(requestURL);
+
+            // Передача запроса далее по конвейеру
+            await _next.Invoke(context);
+        }
+    }
+}
